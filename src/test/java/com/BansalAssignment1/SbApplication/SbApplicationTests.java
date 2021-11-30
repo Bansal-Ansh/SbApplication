eb9d92be00db7431fd9174af293053f5ee6802f3
Bansal-Ansh <ansh.bansal@unthinkable.co>
2021-11-30 16:55:32 +0530

    Intial Commit

---
+++ src/test/java/com/BansalAssignment1/SbApplication/SbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BansalAssignment1.SbApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
